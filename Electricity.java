63a57de34c3f98b1a2b3ad18ef5816f4b1591c71
Ashwinkumar <89375519+Ashwinpalani@users.noreply.github.com>
2022-09-27 23:11:01 +0530

    Add files via upload

---
+++ Electricity.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Electricity
+{ 
+    private double res;
+    private void minbill(double unit)
+    {
+        System.out.println("Electricity bill: Free");
+    }
+    class Bill{
+        //double res;
+        void maxbill(double unit)
+        {
+            if(unit<=200)
+            {
+                res = 100*0 + (unit-100)*2;
+                System.out.println("Electricity bill:"+res);
+            }
+            else if(unit<300)
+            {
+                res = (100*0) + (200*2) + (unit - 200)*3; 
+                System.out.println("Electricity bill:"+res);
+            }
+            else if(unit > 300)
+            {
+                
+                 res = 100*0 + 200*2 +(unit - 300)*4;  
+                  System.out.println("Electricity bill:"+res);
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the units:");
+        double unit = sc.nextDouble(); 
+        Electricity e = new Electricity();
+        Electricity.Bill b = e.new Bill();
+        if(unit <= 100)
+        {
+          e.minbill(unit);
+        }
+        else if(unit > 100)
+        {
+          b.maxbill(unit);
+        }
+    }
+}
